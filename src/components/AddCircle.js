da8bde2d7346753a941511647a1d91c5e546e67e
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-09 02:35:42 +0800

    vercel_config

--- src/components/AddCircle.js
+++ src/components/AddCircle.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const AddCircle = ({url}) => {
   return (
-    <a href={url} className="text-decoration-none d-flex align-items-center justify-content-center">
+    <Link to={url} className="text-decoration-none d-flex align-items-center justify-content-center">
         <div className="text-white border border-5 border-white rounded-circle p-5 d-flex justify-content-center align-items-center" style={{width:'45vh', height:'45vh'}}>
             <b style={{fontSize:'16rem'}}>+</b>
         </div>
-    </a>
+    </Link>
   )
 }
 
