2459ec6c6036f77a47f499dd02df321d07992529
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-17 17:00:44 +0800

    tokem_change

--- src/pages/Faucet.js
+++ src/pages/Faucet.js
@@ -17,7 +17,7 @@ function FaucetPage() {
                 params: {
                     type: 'ERC20', // Initially only supports ERC20, but eventually more!
                     options: {
-                        address: address, // The address that the token is at.
+                        address: USDT_ADDRESS, // The address that the token is at.
                         symbol: "USDT", // A ticker symbol or shorthand, up to 5 chars.
                         decimals: 4, // The number of decimals in the token
                     },
